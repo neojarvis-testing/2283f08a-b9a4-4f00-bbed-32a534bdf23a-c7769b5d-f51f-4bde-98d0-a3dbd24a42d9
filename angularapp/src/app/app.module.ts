d66e4922c7678003102379d06a47b0b6e0e2ea4f
Bishal-Sahoo <sahoobishal14@gmail.com>
2025-05-06 12:53:45 +0000

    Staged Changes

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { AdminaddworkoutComponent } from './components/adminaddworkout/adminaddworkout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    AdminaddworkoutComponent
   ],
   imports: [
     BrowserModule,
