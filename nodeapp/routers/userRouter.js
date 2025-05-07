1e95f3eb043a09122c5618b938d32edc32365aa9
Venumadhavi30 <venumadhavi21114@gmail.com>
2025-05-07 18:29:42 +0000

    Team-2:UserRouter

--- nodeapp/routers/userRouter.js
+++ nodeapp/routers/userRouter.js
@@ -2,14 +2,6 @@ const express=require('express')
 const { addUser, getUserByEmailAndPassword } = require('../controllers/userController')
 const router=express.Router()
 
-<<<<<<< HEAD
-const {getUserByEmailAndPassword,addUser}=require('../controllers/userController')
-
 router.post('/signup',addUser)
 router.post('/login',getUserByEmailAndPassword)
-
-=======
-router.post('/signup',addUser)
-router.post('/login',getUserByEmailAndPassword)
->>>>>>> main
 module.exports=router
