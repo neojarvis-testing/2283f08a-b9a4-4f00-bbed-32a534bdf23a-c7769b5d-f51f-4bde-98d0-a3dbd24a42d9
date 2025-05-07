b65e720dc8fb8eb76bba16481803adfbd06539f4
Bishal-Sahoo <sahoobishal14@gmail.com>
2025-05-07 18:46:21 +0000

    Updated login component

--- angularapp/src/app/components/login/login.component.ts
+++ angularapp/src/app/components/login/login.component.ts
@@ -9,17 +9,6 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-<<<<<<< HEAD
-loginForm:FormGroup;
-submitted=false;
-loading=false;
-loginError='';
-  constructor(private fb: FormBuilder,private router:Router,private authService: AuthService) { 
-  this.loginForm=this.fb.group({
-    email:['',[Validators.required,Validators.pattern('^[a-z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')]],
-    password:['',[Validators.required,Validators.pattern('^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$')]]
-  })
-=======
   loginForm: FormGroup;
   submitted = false;
   constructor(private fb: FormBuilder, private router: Router, private authService: AuthService) {
@@ -27,7 +16,6 @@ loginError='';
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.pattern('^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$')]]
     })
->>>>>>> main
 
   }
   get f() {
@@ -41,12 +29,6 @@ loginError='';
     const { email, password } = this.loginForm.value;
 
     this.authService.login({ email, password }).subscribe({
-<<<<<<< HEAD
-      next: () => {
-          this.router.navigate(['/home-page']);
-      },
-=======
->>>>>>> main
       error: (error) => {
         console.error('Login failed:', error);
         alert('Login failed. Please try again.');
